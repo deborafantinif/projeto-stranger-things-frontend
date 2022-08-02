0b5045108d51404af15567d993488a03a4ddab44
deborafantinif <deboraferfan@gmail.com>
2022-08-02 10:43:35 -0300

    feat: adiciona app em produção

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
+      { (process.env.REACT_APP_IN_PRODUCTION === 'false')
+        ? <div>Em desenvolvimento</div> : '' }
       <StrangerThings />
     </div>
   );
